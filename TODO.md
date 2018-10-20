83e51f19b92aad1a6476623be2e32119c5c7204b
ben <beanie@benle.de>
2018-10-20 13:33:19 +0200

    Project structure and connect to wifi

---
+++ TODO.md
@@ -0,0 +1,8 @@
+getPowerState: function (callback) {
+setPowerState: function (on, callback) {
+getBrightness: function (callback) {
+setBrightness: function (brightness, callback) {
+getHue: function (callback) {
+setHue: function (hue, callback) {
+getSaturation: function (callback) {
+setSaturation: function (saturation, callback) {
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("Internet of Shit", "noaccessforyou");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
