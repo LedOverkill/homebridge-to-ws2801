938e8e1f7b5ea2fbe2a268cd8c3c43f0156e85be
Kristin Fritsch <ich@kristin-fritsch.de>
2018-10-20 13:53:02 +0200

    Add example animation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,56 @@
+/*
+ * Project ledcontroller
+ * Description: control the led strip over tcp
+ * Author: Kristin and Benjamin Fritsch
+ * Date: 10/16/18
+ */
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include "Adafruit_WS2801.h"
+
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+// The colors of the wires may be totally different so
+// BE SURE TO CHECK YOUR PIXELS TO SEE WHICH WIRES TO USE!
+
+// Mapping pin to GPIO pins on NodeMCU
+// static const uint8_t D0   = 16;
+// static const uint8_t D1   = 5;
+// static const uint8_t D2   = 4;
+// static const uint8_t D3   = 0;
+// static const uint8_t D4   = 2;
+// static const uint8_t D5   = 14;
+// static const uint8_t D6   = 12;
+// static const uint8_t D7   = 13;
+// static const uint8_t D8   = 15;
+// static const uint8_t D9   = 3;
+// static const uint8_t D10  = 1;
+
+uint8_t dataPin  = D2;    // Yellow wire on Adafruit Pixels
+uint8_t clockPin = D3;    // Green wire on Adafruit Pixels
+
+// Don't forget to connect the ground wire to Arduino ground,
+// and the +5V wire to a +5V supply
+
+uint32_t Color(byte r, byte g, byte b);
+void colorWipe(uint32_t c, uint8_t wait);
+
+// Set the first variable to the NUMBER of pixels. 25 = 25 pixels in a row
+Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin);
+
+// Optional: leave off pin numbers to use hardware SPI
+// (pinout is then specific to each board and can't be changed)
+//Adafruit_WS2801 strip = Adafruit_WS2801(25);
+
+// For 36mm LED pixels: these pixels internally represent color in a
+// different format.  Either of the above constructors can accept an
+// optional extra parameter: WS2801_RGB is 'conventional' RGB order
+// WS2801_GRB is the GRB order required by the 36mm pixels.  Other
+// than this parameter, your code does not need to do anything different;
+// the library will handle the format change.  Examples:
+//Adafruit_WS2801 strip = Adafruit_WS2801(25, dataPin, clockPin, WS2801_GRB);
+//Adafruit_WS2801 strip = Adafruit_WS2801(25, WS2801_GRB);
 
 void setup() {
   Serial.begin(115200);
@@ -17,8 +68,42 @@ void setup() {
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  strip.begin();
+
+  // Update LED contents, to start they are all 'off'
+  strip.show();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  // Some example procedures showing how to display to the pixels
+  colorWipe(Color(255, 0, 0), 50);
+  colorWipe(Color(0, 255, 0), 50);
+  colorWipe(Color(0, 0, 255), 50);
+}
+
+// fill the dots one after the other with said color
+// good for testing purposes
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+  
+  for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+/* Helper functions */
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
 }
